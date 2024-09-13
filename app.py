bf2e8535fbc116bd7a220b73159a06117dbebb5f
unknown <puddino1337@gmail.com>
2024-09-13 10:14:48 +0200

    Secret key management

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+# Imposta la chiave segreta utilizzando una variabile d'ambiente
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
+
+# Verifica se la chiave segreta è impostata, se no, stampa un avviso
+if app.secret_key is None:
+    print("Attenzione: FLASK_SECRET_KEY non è impostata!")
 
 @app.route('/')
 def hello():
